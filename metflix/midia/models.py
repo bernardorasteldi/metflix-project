b04feed4cfc8eb7633b45806f5f94bd7ef7ff843
bernardorasteldi <rasteldi.dev@gmail.com>
2025-05-14 12:07:53 -0300

    fix: making email a non unique attribute

--- metflix/midia/models.py
+++ metflix/midia/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=False, blank=True, null=True)
 
     def __str__(self):
         return self.username
