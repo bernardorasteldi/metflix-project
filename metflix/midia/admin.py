5e7039d44d010ca3e7649d562e565f9415f15c91
Otávio Henrique Gonçalves Ribeiro <otavio.ribeiro@estudante.ufla.br>
2025-05-14 21:35:37 -0300

    Filter and search bar of the administrative environment

--- metflix/midia/admin.py
+++ metflix/midia/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from .models import Usuario, Jogo, FilmeSerie, UsuarioJogo, UsuarioFilmeSerie
 
-admin.site.register(Usuario)
-admin.site.register(Jogo)
-admin.site.register(FilmeSerie)
-admin.site.register(UsuarioJogo)
-admin.site.register(UsuarioFilmeSerie)
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    search_fields = ['username']
+
+@admin.register(Jogo)
+class JogoAdmin(admin.ModelAdmin):
+    search_fields = ['nome']
+    list_display = ['nome', 'data_lancamento', 'preco']
+
+@admin.register(FilmeSerie)
+class FilmeSerieAdmin(admin.ModelAdmin):
+    search_fields = ['nome']
+    list_display = ['nome', 'categoria', 'data_lancamento', 'genero']
+    list_filter = ['categoria']
+
+@admin.register(UsuarioJogo)
+class UsuarioJogoAdmin(admin.ModelAdmin):
+    list_display = ['usuario', 'jogo']
+
+@admin.register(UsuarioFilmeSerie)
+class UsuarioFilmeSerieAdmin(admin.ModelAdmin):
+    list_display = ['usuario', 'filme_serie']
